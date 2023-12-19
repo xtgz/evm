952ca05aac898113105c136e01ba5b3e45cf319f
xtgz <141474535+xtgz@users.noreply.github.com>
2023-12-19 23:45:59 +0700

    t

--- genMnemonic.js
+++ genMnemonic.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const fs = require('fs');
 const bip39 = require('bip39')
-// 生成英文助记词
+// Generate English mnemonics
 const mnemonicEn = bip39.generateMnemonic()
-// 生成中文助记词
+// Generate Chinese mnemonic words
 const mnemonicCn = bip39.generateMnemonic(128, null, bip39.wordlists.chinese_simplified);
-console.log('英文助记词：' + mnemonicEn);
-console.log('中文助记词：' + mnemonicCn);
+console.log('English mnemonic: ' + mnemonicEn);
+console.log('Chinese mnemonic: ' + mnemonicCn);
 const mnemonicInfo = {
-	英文助记词: mnemonicEn,
-	中文助记词: mnemonicCn
+	English mnemonic: mnemonicEn,
+	Chinese mnemonic: mnemonicCn
 };
-// 将助记词写入 JSON 文件
+//Write mnemonic words into JSON file
 fs.writeFileSync('mnemonic.json', JSON.stringify(mnemonicInfo, null, 2));
