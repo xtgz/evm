5a55a3118d567807cda2262d487838baf46eb04c
xtgz <141474535+xtgz@users.noreply.github.com>
2023-12-19 23:46:27 +0700

    t

--- fastMint.js
+++ fastMint.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 const {Web3} = require('web3');
 const fs = require('fs');
 
-const web3 = new Web3(process.env.NODE_URL); // 节点URL
-const amount = web3.utils.toWei('0', 'ether'); // 期望交易金额，单位为wei
+const web3 = new Web3(process.env.NODE_URL); // Node URL
+const amount = web3.utils.toWei('0', 'ether'); // Expected transaction amount, unit is wei
 const mintStr = process.env.MINT_STR;
-// 将字符串转换为UTF-8编码的字节
+//Convert string to UTF-8 encoded bytes
 const utf8Bytes = Buffer.from(mintStr, 'utf-8');
-// 将字节转换为十六进制表示的字符串
+// Convert bytes to hexadecimal representation of string
 const hexStr = '0x' + utf8Bytes.toString('hex');
 const totalTimes = BigInt(process.env.NUMBER_OF_TIMES);
 
-// 直到出错再获取gas和nonce，不管交易结果，一直发送交易
+// Get the gas and nonce until an error occurs, and keep sending the transaction regardless of the transaction result.
 async function performTransaction(walletInfo, numberOfTimes) {
     const nonce = await web3.eth.getTransactionCount(walletInfo.address);
     const gasPrice = await web3.eth.getGasPrice();
@@ -29,16 +29,16 @@ async function performTransaction(walletInfo, numberOfTimes) {
                 value: amount,
                 gasPrice: gasPrice,
                 nonce: nonce + numberOfFinish - BigInt(1),
-                gas: gasEstimate,
+                gas: gasEstimated,
                 data: hexStr,
             };
 
             const signedTransaction = await web3.eth.accounts.signTransaction(transaction, walletInfo.privateKey);
             web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
 
-            console.log(`第 ${walletInfo.num} 个地址 ${walletInfo.address} 第 ${numberOfFinish} 次操作成功.`);
+            console.log(`The ${walletInfo.num}th address ${walletInfo.address} the ${numberOfFinish} operation was successful.`);
         } catch (error) {
-            console.error(`第 ${walletInfo.num} 个地址 ${walletInfo.address} 第 ${numberOfFinish} 次操作失败: `, error);
+            console.error(`The ${walletInfo.num}th address ${walletInfo.address} the ${numberOfFinish}th operation failed: `, error);
             await performTransaction(walletInfo, totalTimes - numberOfFinish);
         }
     }
@@ -49,15 +49,15 @@ async function main() {
     try {
         walletData = JSON.parse(fs.readFileSync('evm_wallets.json', 'utf-8'));
     } catch (e) {
-        console.log('未找到 evm_wallets.json 文件');
+        console.log('evm_wallets.json file not found');
     }
 
     Promise.all(walletData.map(wallet => performTransaction(wallet, totalTimes)))
         .then(() => {
-            console.log("所有操作完成");
+            console.log("All operations completed");
         })
         .catch(error => {
-            console.error("操作中有错误发生: ", error);
+            console.error("An error occurred during operation: ", error);
         });
 }
 
